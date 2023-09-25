2287d264cb5d3be99d404ef266d69f37bbdacbe5
Mahmut Alperen Ünal <mahmutalperenunal@gmail.com>
2023-09-25 22:01:57 +0300

    chatting activity design

---
+++ app/src/main/java/com/mahmutalperenunal/chatapp/ui/ChattingActivity.kt
@@ -0,0 +1,12 @@
+package com.mahmutalperenunal.chatapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahmutalperenunal.chatapp.R
+
+class ChattingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting)
+    }
+}
