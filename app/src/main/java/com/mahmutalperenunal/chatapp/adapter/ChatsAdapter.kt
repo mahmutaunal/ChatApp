90292688ad35ab8cd70e99e1812674668dd42400
Mahmut Alperen Ünal <mahmutalperenunal@gmail.com>
2023-09-27 20:19:15 +0300

    delete messages and view full screen images

--- app/src/main/java/com/mahmutalperenunal/chatapp/adapter/ChatsAdapter.kt
+++ app/src/main/java/com/mahmutalperenunal/chatapp/adapter/ChatsAdapter.kt
@@ -2,18 +2,23 @@ package com.mahmutalperenunal.chatapp.adapter
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import com.mahmutalperenunal.chatapp.R
 import com.mahmutalperenunal.chatapp.model.Chat
+import com.mahmutalperenunal.chatapp.ui.ViewFullImageActivity
 
 class ChatsAdapter(
     private var mContext: Context,
@@ -61,19 +66,89 @@ class ChatsAdapter(
         if (chat.message.equals("Sent you an image.") && !chat.url.equals("")) {
 
             if (chat.sender.equals(firebaseUser.uid)) {
+
                 holder.message!!.visibility = View.GONE
                 holder.imageMessageRight!!.visibility = View.VISIBLE
                 Glide.with(mContext).load(chat.url).centerCrop()
                     .placeholder(R.drawable.profile_image).into(holder.imageMessageRight!!)
+
+                holder.imageMessageRight!!.setOnClickListener {
+                    val options = arrayOf<CharSequence>(
+                        "View Full Image",
+                        "Delete Image",
+                        "Cancel"
+                    )
+
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(mContext, R.style.CustomAlertDialog)
+                    builder.setTitle("What do you want?")
+
+                    builder.setItems(options) { _, which ->
+                        if (which == 0) {
+                            val intent = Intent(mContext, ViewFullImageActivity::class.java)
+                            intent.putExtra("url", chat.url)
+                            mContext.startActivity(intent)
+                        } else if (which == 1) {
+                            deleteMessage(position, holder)
+                        }
+                    }
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
+                }
+
+
             } else if (!chat.sender.equals(firebaseUser.uid)) {
+
                 holder.message!!.visibility = View.GONE
                 holder.imageMessageLeft!!.visibility = View.VISIBLE
                 Glide.with(mContext).load(chat.url).centerCrop()
                     .placeholder(R.drawable.profile_image).into(holder.imageMessageLeft!!)
+
+                holder.imageMessageLeft!!.setOnClickListener {
+                    val options = arrayOf<CharSequence>(
+                        "View Full Image",
+                        "Cancel"
+                    )
+
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(holder.itemView.context, R.style.CustomAlertDialog)
+                    builder.setTitle("What do you want?")
+
+                    builder.setItems(options) { _, which ->
+                        if (which == 0) {
+                            val intent = Intent(mContext, ViewFullImageActivity::class.java)
+                            intent.putExtra("url", chat.url)
+                            mContext.startActivity(intent)
+                        }
+                    }
+                    builder.show()
+                }
+
             }
 
         } else {
+
             holder.message!!.text = chat.message
+
+            if (firebaseUser.uid == chat.sender) {
+                holder.message!!.setOnClickListener {
+                    val options = arrayOf<CharSequence>(
+                        "Delete Message",
+                        "Cancel"
+                    )
+
+                    AlertDialog.Builder(holder.itemView.context, R.style.CustomAlertDialog)
+                        .setTitle("What do you want?")
+                        .setPositiveButton("Delete Message") { dialog, _ ->
+                            deleteMessage(position, holder)
+                            dialog.dismiss()
+                        }
+                        .setNegativeButton("Cancel") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                    .create()
+                    .show()
+                }
+            }
+
         }
 
         if (position == mChatList.size - 1) {
@@ -113,6 +188,24 @@ class ChatsAdapter(
         } else {
             0
         }
+    }
+
+    private fun deleteMessage(position: Int, holder: ChatsAdapter.ViewHolder) {
+        FirebaseDatabase.getInstance().reference.child("Chats")
+            .child(mChatList[position].messageId!!)
+            .removeValue()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(holder.itemView.context, "Message Deleted.", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(
+                        holder.itemView.context,
+                        "Failed, Message Not Deleted!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
 }
---
+++ app/src/main/java/com/mahmutalperenunal/chatapp/ui/ViewFullImageActivity.kt
@@ -0,0 +1,21 @@
+package com.mahmutalperenunal.chatapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.mahmutalperenunal.chatapp.R
+import com.mahmutalperenunal.chatapp.databinding.ActivityViewFullImageBinding
+
+class ViewFullImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewFullImageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewFullImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageUrl = intent.getStringExtra("url")
+        Glide.with(applicationContext).load(imageUrl).into(binding.viewFullImageImageView)
+    }
+}
